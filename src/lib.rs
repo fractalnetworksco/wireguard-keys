b9c355163a99d30e0fe234681b36450556a4f494
Patrick Elsen <pelsen@xfbs.net>
2021-12-17 01:02:28 +0100

    Implement custom serde serialization and deserialization, remove duplicated code, make a lot of features optional.

--- src/lib.rs
+++ src/lib.rs
@@ -1,96 +1,72 @@
+#[macro_use]
+mod macros;
+
+use paste::paste;
 use rand_core::{OsRng, RngCore};
 #[cfg(feature = "rocket")]
 use rocket::request::FromParam;
 #[cfg(feature = "schema")]
 use schemars::JsonSchema;
 #[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
-use std::fmt::{Display, Formatter};
+use serde::{
+    de::{Error, Visitor},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
+#[cfg(feature = "serde")]
+use std::fmt;
 use std::str::FromStr;
 use thiserror::Error;
 use x25519_dalek_fiat::{PublicKey, StaticSecret};
 
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "schema", derive(JsonSchema))]
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Pubkey([u8; 32]);
-
 #[derive(Error, Debug)]
 pub enum ParseError {
+    #[cfg(feature = "base64")]
     #[error("base64 decoding error")]
     Base64(#[from] base64::DecodeError),
+    #[cfg(feature = "hex")]
     #[error("hex decoding errro")]
     Hex(#[from] hex::FromHexError),
+    #[cfg(feature = "base32")]
+    #[error("base32 decoding error")]
+    Base32Error,
     #[error("length mismatch")]
     Length,
 }
 
+/// Length (in bytes) of a WireGuard public key (ed25519).
 const PUBKEY_LEN: usize = 32;
+
+/// Length (in bytes) of a WireGuard private key (ed25519).
 const PRIVKEY_LEN: usize = 32;
+
+/// Length (in bytes) of a WireGuard preshared key.
 const SECRET_LEN: usize = 32;
 
+/// WireGuard public key.
+#[cfg_attr(feature = "schema", derive(JsonSchema))]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Pubkey([u8; PUBKEY_LEN]);
+
+impl_new!(Pubkey, PUBKEY_LEN);
+impl_deref!(Pubkey, PUBKEY_LEN);
+#[cfg(feature = "hex")]
+impl_hex!(Pubkey);
+#[cfg(feature = "base64")]
+impl_base64!(Pubkey);
+#[cfg(feature = "base32")]
+impl_base32!(Pubkey);
+impl_parse!(Pubkey);
+
+#[cfg(feature = "serde")]
+impl_serde!(Pubkey, "WireGuard public key");
+
+#[cfg(feature = "rocket")]
+impl_rocket!(Pubkey);
+
 impl Pubkey {
-    pub fn new(data: [u8; PUBKEY_LEN]) -> Self {
-        Pubkey(data)
-    }
-
-    pub fn raw(self) -> [u8; PUBKEY_LEN] {
-        self.0
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.0[..]
-    }
-
-    pub fn from_base64(data: &str) -> Result<Self, ParseError> {
-        let data = base64::decode(data)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn from_base64_urlsafe(data: &str) -> Result<Self, ParseError> {
-        let data = base64::decode_config(data, base64::URL_SAFE)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn from_hex(data: &str) -> Result<Self, ParseError> {
-        let data = hex::decode(data)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn to_hex(&self) -> String {
-        hex::encode(self.0)
-    }
-
-    pub fn parse(data: &str) -> Result<Self, ParseError> {
-        let ret = match data.len() {
-            64 => Self::from_hex(data)?,
-            44 => Self::from_base64(data).or_else(|_| Self::from_base64_urlsafe(data))?,
-            _ => Err(ParseError::Length)?,
-        };
-        Ok(ret)
-    }
-}
-
-#[test]
-fn test_pubkey_parse() {
-    let pubkey = Privkey::generate().pubkey();
-    let mut pubkey_hex = pubkey.to_hex();
-    assert_eq!(Pubkey::parse(&pubkey_hex).unwrap(), pubkey);
-    let pubkey_base64 = pubkey.to_base64();
-    assert_eq!(Pubkey::parse(&pubkey_base64).unwrap(), pubkey);
-    let pubkey_base64_url = pubkey.to_base64_urlsafe();
-    assert_eq!(Pubkey::parse(&pubkey_base64_url).unwrap(), pubkey);
-}
-
-#[test]
-fn test_pubkey_parse_invalid() {
-    match Pubkey::parse("") {
-        Err(ParseError::Length) => {}
-        _ => assert!(false),
-    }
-    match Pubkey::parse("abc") {
-        Err(ParseError::Length) => {}
-        _ => assert!(false),
+    #[cfg(test)]
+    fn generate() -> Pubkey {
+        Privkey::generate().pubkey()
     }
 }
 
@@ -103,7 +79,7 @@ fn test_pubkey_from_slice() {
     }
     let slice = [0; PUBKEY_LEN];
     match Pubkey::try_from(&slice[..]) {
-        Ok(pubkey) => {}
+        Ok(_) => {}
         _ => assert!(false),
     }
 }
@@ -121,66 +97,35 @@ impl TryFrom<&[u8]> for Pubkey {
     }
 }
 
-impl TryFrom<&str> for Pubkey {
-    type Error = ParseError;
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        Pubkey::parse(value)
-    }
-}
-
-#[cfg(feature = "rocket")]
-impl<'r> FromParam<'r> for Pubkey {
-    type Error = ParseError;
-
-    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
-        Pubkey::parse(param)
-    }
-}
-
-impl FromStr for Pubkey {
-    type Err = ParseError;
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        // try decoding as base64
-        let data = base64::decode(value);
-
-        // if that fails, try decoding as url-safe base64
-        let data = data.or_else(|_| base64::decode_config(value, base64::URL_SAFE));
-
-        let data = data?;
-
-        // make sure the length fits
-        if data.len() != PUBKEY_LEN {
-            return Err(ParseError::Length);
-        }
-        let mut key = [0; PUBKEY_LEN];
-        key.copy_from_slice(&data);
-        Ok(Pubkey(key))
-    }
-}
-
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+/// WireGuard private key.
 #[cfg_attr(feature = "schema", derive(JsonSchema))]
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Privkey([u8; PRIVKEY_LEN]);
 
+impl_new!(Privkey, PRIVKEY_LEN);
+impl_deref!(Privkey, PRIVKEY_LEN);
+#[cfg(feature = "hex")]
+impl_hex!(Privkey);
+#[cfg(feature = "base64")]
+impl_base64!(Privkey);
+#[cfg(feature = "base32")]
+impl_base32!(Privkey);
+impl_parse!(Privkey);
+
+#[cfg(feature = "serde")]
+impl_serde!(Privkey, "WireGuard private key");
+
+#[cfg(feature = "rocket")]
+impl_rocket!(Privkey);
+
 impl Privkey {
-    pub fn new(data: [u8; PRIVKEY_LEN]) -> Self {
-        Privkey(data)
-    }
-
-    pub fn raw(self) -> [u8; PRIVKEY_LEN] {
-        self.0
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.0[..]
-    }
-
+    /// Generate new private key using the kernel randomness generator.
     pub fn generate() -> Self {
         let private_key = StaticSecret::new(OsRng);
         Privkey(private_key.to_bytes())
     }
 
+    /// Attempt to check if this private key is valid.
     pub fn valid(&self) -> bool {
         if self.0 == [0; PRIVKEY_LEN] {
             return false;
@@ -190,61 +135,11 @@ impl Privkey {
         self.0 == private_key.to_bytes()
     }
 
+    /// Generate the corresponding public key for this private key.
     pub fn pubkey(&self) -> Pubkey {
         let private_key = StaticSecret::from(self.0.clone());
         let public_key: PublicKey = (&private_key).into();
         Pubkey(public_key.to_bytes())
-    }
-
-    pub fn from_base64(data: &str) -> Result<Self, ParseError> {
-        let data = base64::decode(data)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn from_base64_urlsafe(data: &str) -> Result<Self, ParseError> {
-        let data = base64::decode_config(data, base64::URL_SAFE)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn from_hex(data: &str) -> Result<Self, ParseError> {
-        let data = hex::decode(data)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn parse(data: &str) -> Result<Self, ParseError> {
-        let ret = match data.len() {
-            64 => Self::from_hex(data)?,
-            44 => Self::from_base64(data).or_else(|_| Self::from_base64_urlsafe(data))?,
-            _ => Err(ParseError::Length)?,
-        };
-        Ok(ret)
-    }
-
-    pub fn to_hex(&self) -> String {
-        hex::encode(self.0)
-    }
-}
-
-#[test]
-fn test_privkey_parse() {
-    let privkey = Privkey::generate();
-    let mut privkey_hex = privkey.to_hex();
-    assert_eq!(Privkey::parse(&privkey_hex).unwrap(), privkey);
-    let privkey_base64 = privkey.to_base64();
-    assert_eq!(Privkey::parse(&privkey_base64).unwrap(), privkey);
-    let privkey_base64_url = privkey.to_base64_urlsafe();
-    assert_eq!(Privkey::parse(&privkey_base64_url).unwrap(), privkey);
-}
-
-#[test]
-fn test_privkey_parse_invalid() {
-    match Privkey::parse("") {
-        Err(ParseError::Length) => {}
-        _ => assert!(false),
-    }
-    match Privkey::parse("abc") {
-        Err(ParseError::Length) => {}
-        _ => assert!(false),
     }
 }
 
@@ -257,7 +152,7 @@ fn test_privkey_from_slice() {
     }
     let slice = [0; PRIVKEY_LEN];
     match Privkey::try_from(&slice[..]) {
-        Ok(pubkey) => {}
+        Ok(_) => {}
         _ => assert!(false),
     }
 }
@@ -275,29 +170,6 @@ impl TryFrom<&[u8]> for Privkey {
     }
 }
 
-impl TryFrom<&str> for Privkey {
-    type Error = ParseError;
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        Privkey::parse(value)
-    }
-}
-
-impl FromStr for Privkey {
-    type Err = ParseError;
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        Privkey::parse(value)
-    }
-}
-
-#[cfg(feature = "rocket")]
-impl<'r> FromParam<'r> for Privkey {
-    type Error = ParseError;
-
-    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
-        Privkey::parse(param)
-    }
-}
-
 #[test]
 fn test_wireguard_privkey() {
     let key = Privkey::new([0; PRIVKEY_LEN]);
@@ -310,79 +182,33 @@ fn test_wireguard_privkey() {
     assert_eq!(key.pubkey(), key.pubkey());
 }
 
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+/// WireGuard preshared key.
 #[cfg_attr(feature = "schema", derive(JsonSchema))]
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Secret([u8; SECRET_LEN]);
 
+impl_new!(Secret, SECRET_LEN);
+impl_deref!(Secret, SECRET_LEN);
+#[cfg(feature = "hex")]
+impl_hex!(Secret);
+#[cfg(feature = "base64")]
+impl_base64!(Secret);
+#[cfg(feature = "base32")]
+impl_base32!(Secret);
+impl_parse!(Secret);
+
+#[cfg(feature = "serde")]
+impl_serde!(Secret, "WireGuard preshared key");
+
+#[cfg(feature = "rocket")]
+impl_rocket!(Secret);
+
 impl Secret {
-    pub fn new(data: [u8; SECRET_LEN]) -> Self {
-        Secret(data)
-    }
-
-    pub fn raw(self) -> [u8; SECRET_LEN] {
-        self.0
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.0[..]
-    }
-
+    /// Generate new random preshared key using the system randomness generator.
     pub fn generate() -> Self {
         let mut data = [0; SECRET_LEN];
         OsRng.fill_bytes(&mut data);
         Secret(data)
-    }
-
-    pub fn from_base64(data: &str) -> Result<Self, ParseError> {
-        let data = base64::decode(data)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn from_base64_urlsafe(data: &str) -> Result<Self, ParseError> {
-        let data = base64::decode_config(data, base64::URL_SAFE)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn from_hex(data: &str) -> Result<Self, ParseError> {
-        let data = hex::decode(data)?;
-        Ok(data.as_slice().try_into()?)
-    }
-
-    pub fn parse(data: &str) -> Result<Self, ParseError> {
-        let ret = match data.len() {
-            64 => Self::from_hex(data)?,
-            44 => Self::from_base64(data).or_else(|_| Self::from_base64_urlsafe(data))?,
-            _ => Err(ParseError::Length)?,
-        };
-        Ok(ret)
-    }
-
-    pub fn to_hex(&self) -> String {
-        hex::encode(self.0)
-    }
-}
-
-#[test]
-fn test_secret_parse() {
-    let secret = Secret::generate();
-    let mut secret_hex = secret.to_hex();
-    assert_eq!(Secret::parse(&secret_hex).unwrap(), secret);
-    let secret_base64 = secret.to_base64();
-    assert_eq!(Secret::parse(&secret_base64).unwrap(), secret);
-    let secret_base64_url = secret.to_base64_urlsafe();
-    assert_eq!(Secret::parse(&secret_base64_url).unwrap(), secret);
-}
-
-#[test]
-fn test_secret_parse_invalid() {
-    match Secret::parse("") {
-        Err(ParseError::Length) => {}
-        _ => assert!(false),
-    }
-    match Secret::parse("abc") {
-        Err(ParseError::Length) => {}
-        _ => assert!(false),
     }
 }
 
@@ -395,7 +221,7 @@ fn test_secret_from_slice() {
     }
     let slice = [0; PRIVKEY_LEN];
     match Secret::try_from(&slice[..]) {
-        Ok(pubkey) => {}
+        Ok(_) => {}
         _ => assert!(false),
     }
 }
@@ -410,76 +236,5 @@ impl TryFrom<&[u8]> for Secret {
             data[0..PUBKEY_LEN].copy_from_slice(&key[0..PUBKEY_LEN]);
             Ok(Secret(data))
         }
-    }
-}
-
-impl TryFrom<&str> for Secret {
-    type Error = ParseError;
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        Secret::parse(value)
-    }
-}
-
-impl FromStr for Secret {
-    type Err = ParseError;
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        Secret::parse(value)
-    }
-}
-
-#[cfg(feature = "rocket")]
-impl<'r> FromParam<'r> for Secret {
-    type Error = ParseError;
-
-    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
-        Secret::parse(param)
-    }
-}
-
-pub trait ToBase64 {
-    fn data(&self) -> &[u8];
-
-    fn to_base64(&self) -> String {
-        base64::encode(self.data())
-    }
-
-    fn to_base64_urlsafe(&self) -> String {
-        base64::encode_config(&self.data(), base64::URL_SAFE)
-    }
-}
-
-impl ToBase64 for Secret {
-    fn data(&self) -> &[u8] {
-        self.as_slice()
-    }
-}
-
-impl ToBase64 for Privkey {
-    fn data(&self) -> &[u8] {
-        self.as_slice()
-    }
-}
-
-impl ToBase64 for Pubkey {
-    fn data(&self) -> &[u8] {
-        self.as_slice()
-    }
-}
-
-impl Display for Secret {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_base64())
-    }
-}
-
-impl Display for Pubkey {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_base64())
-    }
-}
-
-impl Display for Privkey {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_base64())
     }
 }
---
+++ src/macros.rs
@@ -0,0 +1,240 @@
+#[cfg(feature = "serde")]
+macro_rules! impl_serde {
+    ($type:ty, $mesg:literal) => {
+        impl Serialize for $type {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: Serializer,
+            {
+                if serializer.is_human_readable() {
+                    let encoded: String = self.to_base64();
+                    (&encoded).serialize(serializer)
+                } else {
+                    self.0.serialize(serializer)
+                }
+            }
+        }
+
+        impl<'de> Deserialize<'de> for $type {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                if deserializer.is_human_readable() {
+                    struct KeyVisitor;
+
+                    impl<'de> Visitor<'de> for KeyVisitor {
+                        type Value = $type;
+
+                        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                            formatter.write_str($mesg)
+                        }
+
+                        fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+                        where
+                            E: Error,
+                        {
+                            <$type>::from_str(s).map_err(Error::custom)
+                        }
+                    }
+
+                    deserializer.deserialize_str(KeyVisitor)
+                } else {
+                    let data: [u8; 32] = <[u8; 32]>::deserialize(deserializer)?;
+                    Ok(<$type>::new(data))
+                }
+            }
+        }
+
+        paste! {
+            #[test]
+            fn [<test_ $type:lower _serde>]() {
+                use serde_test::{assert_tokens, Configure, Token};
+                let example = "yG+Xc4BmcF/j5ChWkOloirX6nWxjWqN3p2nihDtGVW4=";
+                let key = <$type>::from_str(example).unwrap();
+                assert_tokens(&key.readable(), &[Token::Str(example)]);
+                let mut tokens = vec![Token::Tuple { len: 32 }];
+                for byte in &key.0 {
+                    tokens.push(Token::U8(*byte));
+                }
+                tokens.push(Token::TupleEnd);
+                assert_tokens(&key.compact(), &tokens);
+            }
+        }
+    };
+}
+
+#[cfg(feature = "hex")]
+macro_rules! impl_hex {
+    ($type:ty) => {
+        impl $type {
+            /// Parse key from hex.
+            pub fn from_hex(data: &str) -> Result<Self, ParseError> {
+                let data = hex::decode(data)?;
+                Ok(data.as_slice().try_into()?)
+            }
+
+            /// Encode key as hex.
+            pub fn to_hex(&self) -> String {
+                hex::encode(self.0)
+            }
+        }
+    };
+}
+
+#[cfg(feature = "base32")]
+macro_rules! impl_base32 {
+    ($type:ty) => {
+        impl $type {
+            /// Base32 alphabet to use.
+            const BASE32_ALPHABET: base32::Alphabet = base32::Alphabet::RFC4648 { padding: true };
+
+            /// Parse key from base32.
+            pub fn from_base32(data: &str) -> Result<Self, ParseError> {
+                let data =
+                    base32::decode(Self::BASE32_ALPHABET, data).ok_or(ParseError::Base32Error)?;
+                Ok(data.as_slice().try_into()?)
+            }
+
+            /// Encode key as base32.
+            pub fn to_base32(&self) -> String {
+                base32::encode(Self::BASE32_ALPHABET, &self.0)
+            }
+        }
+    };
+}
+
+#[cfg(feature = "base64")]
+macro_rules! impl_base64 {
+    ($type:ty) => {
+        impl $type {
+            /// Parse key from base64.
+            pub fn from_base64(data: &str) -> Result<Self, ParseError> {
+                let data = base64::decode(data)?;
+                Ok(data.as_slice().try_into()?)
+            }
+
+            /// Parse key from base64 with urlsafe encoding.
+            pub fn from_base64_urlsafe(data: &str) -> Result<Self, ParseError> {
+                let data = base64::decode_config(data, base64::URL_SAFE)?;
+                Ok(data.as_slice().try_into()?)
+            }
+
+            /// Encode key as base64.
+            pub fn to_base64(&self) -> String {
+                base64::encode(&self.0)
+            }
+
+            /// Encode key as base64 with urlsafe encoding.
+            pub fn to_base64_urlsafe(&self) -> String {
+                base64::encode_config(&self.0, base64::URL_SAFE)
+            }
+        }
+    };
+}
+
+macro_rules! impl_parse {
+    ($type:ty) => {
+        impl $type {
+            /// Try parsing from string.
+            pub fn parse(data: &str) -> Result<Self, ParseError> {
+                let ret = match data.len() {
+                    #[cfg(feature = "hex")]
+                    64 => Self::from_hex(data)?,
+                    #[cfg(feature = "base64")]
+                    44 => Self::from_base64(data).or_else(|_| Self::from_base64_urlsafe(data))?,
+                    #[cfg(feature = "base32")]
+                    56 => Self::from_base32(data)?,
+                    _ => Err(ParseError::Length)?,
+                };
+                Ok(ret)
+            }
+        }
+
+        impl TryFrom<&str> for $type {
+            type Error = ParseError;
+            fn try_from(value: &str) -> Result<Self, Self::Error> {
+                <$type>::parse(value)
+            }
+        }
+
+        impl FromStr for $type {
+            type Err = ParseError;
+            fn from_str(value: &str) -> Result<Self, Self::Err> {
+                <$type>::parse(value)
+            }
+        }
+
+        paste! {
+            #[test]
+            fn [<test_ $type:lower _parse>]() {
+                let value = <$type>::generate();
+                #[cfg(feature = "hex")]
+                {
+                    let value_hex = value.to_hex();
+                    assert_eq!(<$type>::parse(&value_hex).unwrap(), value);
+                }
+                #[cfg(feature = "base64")]
+                {
+                    let value_base64 = value.to_base64();
+                    assert_eq!(<$type>::parse(&value_base64).unwrap(), value);
+                    let value_base64_url = value.to_base64_urlsafe();
+                    assert_eq!(<$type>::parse(&value_base64_url).unwrap(), value);
+                }
+                #[cfg(feature = "base32")]
+                {
+                    let value_base32 = value.to_base32();
+                    assert_eq!(<$type>::parse(&value_base32).unwrap(), value);
+                }
+            }
+
+            #[test]
+            fn [<test_ $type:lower _parse_invalid>]() {
+                match <$type>::parse("") {
+                    Err(ParseError::Length) => {}
+                    _ => assert!(false),
+                }
+                match <$type>::parse("abc") {
+                    Err(ParseError::Length) => {}
+                    _ => assert!(false),
+                }
+            }
+        }
+    };
+}
+
+#[cfg(feature = "rocket")]
+macro_rules! impl_rocket {
+    ($type:ty) => {
+        impl<'r> FromParam<'r> for $type {
+            type Error = ParseError;
+
+            fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+                <$type>::parse(param)
+            }
+        }
+    };
+}
+
+macro_rules! impl_new {
+    ($type:ty, $len:expr) => {
+        impl $type {
+            /// Create new from existing data. Warning: this function does not check
+            /// if the passed data is valid.
+            pub fn new(data: [u8; $len]) -> Self {
+                Self(data)
+            }
+        }
+    };
+}
+
+macro_rules! impl_deref {
+    ($type:ty, $len:expr) => {
+        impl std::ops::Deref for $type {
+            type Target = [u8; $len];
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+    };
+}
